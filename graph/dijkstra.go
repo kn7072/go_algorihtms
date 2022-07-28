71ef09ea02bc6915cacd664e36ce3bee311358cf
stepan <kn7072@yandex.ru>
2022-07-28 17:07:31 +0300

    отладка

--- graph/dijkstra.go
+++ graph/dijkstra.go
@@ -2,11 +2,14 @@ package graph
 
 import (
 	"fmt"
-	"github.com/kn7072/GO/algorithm/structure/queue"
+
+	"github.com/kn7072/go_algorihtms/sort"
 )
 
 
 func main() {
-	n := queue.Node{}
-	fmt.Println(n)
+	// n := queue.Node{}
+	fmt.Println(1)
+	x := []int{1, 2, 3}
+	sort.Quicksort(x)
 }
