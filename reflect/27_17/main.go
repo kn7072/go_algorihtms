c3b78966735e7ddc9654e59b4d5a86f48b0315a0
stepan <kn7072@yandex.ru>
2022-10-08 19:36:58 +0300

    reflect

--- reflect/27_17/main.go
+++ reflect/27_17/main.go
@@ -10,9 +10,18 @@ func Printfln(template string, values ...interface{}) {
 	fmt.Printf(template+"\n", values...)
 }
 
+/*
+	Comparable() - Этот метод возвращает true, если отраженный тип можно использовать с
+	оператором сравнения Go, и false в противном случае.
+*/
+
 func contains(slice interface{}, target interface{}) (found bool) {
 	sliceValue := reflect.ValueOf(slice)
-	if sliceValue.Kind() == reflect.Slice {
+	targetType := reflect.TypeOf(target)
+
+	if sliceValue.Kind() == reflect.Slice &&
+		sliceValue.Type().Elem().Comparable() &&
+		targetType.Comparable() {
 		for i := 0; i < sliceValue.Len(); i++ {
 			if sliceValue.Index(i).Interface() == target {
 				found = true
@@ -21,4 +30,16 @@ func contains(slice interface{}, target interface{}) (found bool) {
 	}
 
 	return
+}
+
+func main() {
+	city := "London"
+	citiesSlice := []string{ "Paris", "Rome", "London"}
+
+	Printfln("Found #1: %v", contains(citiesSlice, city))
+
+	sliceOfSlices := [][]string{
+		citiesSlice, {"First", "Second", "Third"}}
+		
+	Printfln("Found #2:	%v", contains(sliceOfSlices, citiesSlice))
 }
