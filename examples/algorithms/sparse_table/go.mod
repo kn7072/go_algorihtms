de0bdc3c29619f9f50f9ca2c9bee3a077c860cd2
stepan <kn7072@yandex.ru>
2022-10-12 20:33:18 +0300

    sparse table

---
+++ examples/algorithms/sparse_table/go.mod
@@ -0,0 +1,3 @@
+module sparse_table
+
+go 1.18
---
+++ examples/algorithms/sparse_table/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func getHeightTable(lenArray int) int {
+	return int(math.Floor(math.Log2(float64(lenArray))))
+}
+
+func setTableMin(sparseTable [][]int) {
+	maxLevel := getHeightTable(len(sparseTable))
+	lenRow := len(sparseTable[0])
+
+	for level := 1; level <= maxLevel+1; level++ {
+		for elemIndex := range sparseTable[level] {
+			rightBorder := elemIndex + int(math.Pow(2, float64(level)))
+			if rightBorder <= lenRow {
+				sparseTable[level][elemIndex] = int(math.Max(float64(sparseTable[level-1][elemIndex]),
+					float64(sparseTable[level-1][elemIndex+int(math.Pow(2, float64(level-1)))])))
+			}
+		}
+	}
+}
+
+func request(sparseTable [][]int, left, right int) int {
+	// нужно добавить проверки границ
+	level := int(math.Floor(math.Log2(float64(right - left))))
+
+	return int(math.Max(float64(sparseTable[level][left]), 
+		float64(sparseTable[level][right-int(math.Pow(2, float64(level)))])))
+}
+
+func createEmptyTable(array []int) [][]int {
+	lenTable := len(array)
+	heightTable := getHeightTable(lenTable) + 1
+	sparseTable := make([][]int, heightTable)
+
+	for i := 0; i < heightTable; i++ {
+		sparseTable[i] = make([]int, lenTable)
+	}
+
+	copy(sparseTable[0], array)
+
+	return sparseTable
+}
+
+func main() {
+	array := []int{2, 4, 5, 8, 9, 1, 3, 7, 10}
+	// fmt.Println(getHeightTable(len(array)))
+
+	sparseTable := createEmptyTable(array)
+
+	setTableMin(sparseTable)
+	// fmt.Println(sparseTable)
+
+	req := request(sparseTable, 1, 5)
+	fmt.Println(req)
+
+	req = request(sparseTable, 2, 3)
+	fmt.Println(req)
+
+	req = request(sparseTable, 0, 1)
+	fmt.Println(req)
+}
