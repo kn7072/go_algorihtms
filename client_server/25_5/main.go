f106edce8fdb428b87cc731b88c2ae42e77f1c31
stepan <kn7072@yandex.ru>
2022-10-23 17:32:27 +0300

    client

--- client_server/25_5/main.go
+++ client_server/25_5/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"time"
@@ -43,16 +45,83 @@ func Printfln(template string, values ...interface{}) {
 		и error, сообщающая о проблемах с запросом.
 */
 
+/*
+Таблица 25-4 Поля и методы, определяемые структурой Response
+	StatusCode - Это поле возвращает код состояния ответа, выраженный как int.
+	Status - Это поле возвращает string, содержащую описание статуса.
+	Proto - Это поле возвращает string, содержащую ответный HTTP-протокол.
+	Header - Это поле возвращает строку map[string][]string, содержащую заголовки ответа.
+	Body - Это поле возвращает ReadCloser, который является Reader,
+		определяющим метод Close и обеспечивающим доступ к телу ответа.
+	Trailer - Это поле возвращает строку map[string][]string, содержащую трейлеры ответов.
+	ContentLength - Это поле возвращает значение заголовка Content-Length,
+		преобразованное в значение int64.
+	TransferEncoding - Это поле возвращает набор значений заголовка Transfer-Encoding.
+	Close - Это логическое поле возвращает значение true, если ответ содержит
+		заголовок Connection, для которого установлено значение close, что
+		указывает на то, что HTTP-соединение должно быть закрыто.
+	Uncompressed - Это поле возвращает значение true, если сервер отправил сжатый ответ,
+		который был распакован пакетом net/http.
+	Request - Это поле возвращает Request, который использовался для получения
+		ответа. Структура Request описана в главе 24.
+	TLS - В этом поле содержится информация о соединении HTTPS.
+	Cookies() - Этот метод возвращает []*Cookie, который содержит заголовки Set-
+		Cookie в ответе. Структура Cookie описана в главе 24.
+	Location - Этот метод возвращает URL-адрес из ответа заголовка Location и error,
+		указывающую, что ответ не содержит этот заголовок.
+	Write(writer) - Этот метод записывает сводку ответа на указанный Writer.
+
+*/
+
 func main() {
 	Printfln("Starting HTTP Server")
 	go http.ListenAndServe(":5000", nil)
-	time.Sleep(time.Second)
+	time.Sleep(3 * time.Second)
 
 	response, err := http.Get("http://localhost:5000/html")
-	if err == nil {
-		response.Write(os.Stdout)
+	if (err == nil && response.StatusCode == http.StatusOK) {
+
+		data, err := io.ReadAll(response.Body)
+
+		if (err == nil) {
+			defer response.Body.Close()
+			os.Stdout.Write(data)
+		}
 	} else {
-		Printfln("Error: %v", err.Error())
+		Printfln("Error: %v, Status Code: %v", err.Error(),
+			response.StatusCode)
+	}
+
+	response, err = http.Get("http://localhost:5000/json")
+	if (err == nil && response.StatusCode == http.StatusOK) {
+		defer response.Body.Close()
+
+		data := []Product{}
+		err = json.NewDecoder(response.Body).Decode(&data)
+
+		if err == nil {
+			for _, p := range data {
+				Printfln("Name: %v, Price: $%.2f", p.Name, p.Price)
+			}
+		} else {
+			Printfln("Decode error: %v", err.Error())
+		}
+	} else {
+		Printfln("Error: %v, Status Code: %v", err.Error(), response.StatusCode)
+	}
+
+	formData := map[string][]string {
+		"name": { "Kayak "},
+		"category": { "Watersports"},
+		"price": { "279"},
+	}
+		
+	response, err = http.PostForm("http://localhost:5000/echo", formData)
+	if (err == nil && response.StatusCode == http.StatusOK) {
+		defer response.Body.Close()
+		io.Copy(os.Stdout, response.Body)
+	} else {
+		Printfln("Error: %v, Status Code: %v", err.Error(),response.StatusCode)
 	}
 
 	// http://localhost:5000/echo
